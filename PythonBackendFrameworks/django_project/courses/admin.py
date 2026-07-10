a8123b40c576b4aef989dcaae012d926fb673794
Malar5717 <malarmariam613@gmail.com>
2026-07-10 23:41:23 +0530

    python backend: django - handson 2

--- PythonBackendFrameworks/django_project/courses/admin.py
+++ PythonBackendFrameworks/django_project/courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Department, Course, Student, Enrollment
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'credits', 'department']
+    search_fields = ['name', 'code']
+    list_filter = ['department']
+
+admin.site.register(Department)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Student)
+admin.site.register(Enrollment)
